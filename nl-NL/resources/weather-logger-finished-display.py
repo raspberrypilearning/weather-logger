c889c2227e519f68d3756425456a7a6b4b500e1d
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-03-12 10:52:49 +0000

    New translations weather-logger-finished-display.py (Dutch)

--- nl-NL/resources/weather-logger-finished-display.py
+++ nl-NL/resources/weather-logger-finished-display.py
@@ -2,16 +2,16 @@ import pygal
 
 temp = []
 
-file = open('weather.txt', 'r')
+bestand = open('weather.txt', 'r')
 
-for line in file.read().splitlines():
+for regel in bestand.read().splitlines():
   if regel:
-    temp.append( float(line) )
-file.close()
+    temp.append( float(regel) )
+bestand.close()
 
-weatherchart = pygal.Line()
-weatherchart.title = 'Weather'
-weatherchart.x_labels = range(1, len(temp) + 1)
-weatherchart.add('temp', temp)
-weatherchart.render()
+weergrafiek = pygal.Line()
+weergrafiek.title = 'Weer'
+weergrafiek.x_labels = range(1, len(temp) + 1)
+weergrafiek.add('temp', temp)
+weergrafiek.render()
 
